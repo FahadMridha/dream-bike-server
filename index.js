176def8cb88a080c2f848e56b911d9f9b3ddc4f7
fahad Mridha <sohelcse16@gmail.com>
2022-11-25 02:49:33 +0600

    mongodb uri setup

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Dream bike server is Running");
+});
+
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const categoryCollection = client
+      .db("dreamBike")
+      .collection("AllCategorys");
+    const productCollection = client.db("dreamBike").collection("AllProducts");
+  } catch (error) {
+    console.log(error);
+  }
+}
+run().catch((error) => {
+  console.log(error);
 });
 
 app.listen(port, console.log(`server is running on port: ${port}`));
